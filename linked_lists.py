9a19eec2c4404817820107e39c25364f69d56f21
Oskar Wieczorek <oziw57@gmail.com>
2025-11-04 10:27:03 +0000

    implimented an insert function

--- linked_lists.py
+++ linked_lists.py
@@ -6,3 +6,13 @@ class Node:
 class SinglyLinkedList:
     def __init__(self):
         self.head = None
+    
+    def insert(self, data):
+        new_node = Node(data)
+        if not self.head: # Special case: if list is empty
+            self.head = new_node
+        else:
+            current = self.head
+            while current.next: # traverse to the last node
+                current = current.next
+            current.next = new_node
